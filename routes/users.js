3adabebd276a2952412c6e36566fc22276a9b401
lisandrozabala <lzabala@udesa.edu.ar>
2024-06-11 18:53:15 -0300

    fixes

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,22 @@
 var express = require('express');
-const usuariosController = require('../controllers/usuariosControllers');
 var router = express.Router();
 const { body } = require('express-validator');
+const usuariosController = require('../controllers/usuariosControllers');
 
-const validations = [
+
+const validaciones = [
     body ("email").notEmpty().isEmail().withMessage("Debes completar el campo de email").bail(),
     body ("nombre").notEmpty().isLength({ min: 3, max: 13 }).withMessage("Debes completar el campo de nombre").bail(),
     body ("contraseña").notEmpty().withMessage("Debes completar el campo de contraseña").bail(),
     body ("nacimiento").notEmpty().withMessage("Debes completar el campo de fecha nacimiento").bail(),
-    body ("number").notEmpty().isLength({ min: 8, max: 8 }).isInt().withMessage("Debes completar el campo de documento").bail()
+    body ("documento").notEmpty().isLength({ min: 8, max: 8 }).isInt().withMessage("Debes completar el campo de documento").bail()
 ]
 
 /* GET users listing. */
 router.get('/profile', usuariosController.profile);
 
 router.get('/register', usuariosController.register);
-router.post('/register', usuariosController.registerInfo);
+router.post('/register', validaciones, usuariosController.registerInfo);
 
 router.get('/login', usuariosController.login);
 router.post('/login', usuariosController.loginInfo);
